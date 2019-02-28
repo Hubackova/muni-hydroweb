5d32502c921d62e53697620b4d87259457072222
Lenka <hubackova.lenka@gmail.com>
2019-02-28 08:11:24 +0100

    courses in two columns

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,22 +1,21 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 import styled from "styled-components";
 import Layout from "../components/layout";
 
 const Courses = ({ data }) => {
-  if (!data && !data.markdownRemark) return <div>...loading</div>;
-  const { markdownRemark: courses } = data;
+  if (!data && !data.allMarkdownRemark) return <div>...loading</div>;
+  const { allMarkdownRemark } = data;
+  const courses1 = allMarkdownRemark.edges[0].node;
+  const courses2 = allMarkdownRemark.edges[1].node;
 
   return (
     <Layout>
-    <Container>
-      <h1> Předměty </h1>
-      <div>
-      <div dangerouslySetInnerHTML={{ __html: courses.html }} />
-    </div>
-    </Container>
-  </Layout>
-
+      <Container>
+        <div dangerouslySetInnerHTML={{ __html: courses1.html }} />
+        <div dangerouslySetInnerHTML={{ __html: courses2.html }} />
+      </Container>
+    </Layout>
   );
 };
 
@@ -24,10 +23,11 @@ export default Courses;
 
 export const coursesQuery = graphql`
   query coursesQuery {
-    markdownRemark(frontmatter: { title: { eq: "courses" } }) {
-      html
-      frontmatter {
-        title
+    allMarkdownRemark {
+      edges {
+        node {
+          html
+        }
       }
     }
   }
@@ -36,8 +36,16 @@ export const coursesQuery = graphql`
 const Container = styled.div`
   width: 90%;
   margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
   @media (max-width: 800px) {
     width: 100%;
+  }
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
+  div {
+    max-width: 40%;
   }
 `;
 
