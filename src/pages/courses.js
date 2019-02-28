8580275455ee5b0bbde2d86bd04ab5a7e487ad98
Lenka <hubackova.lenka@gmail.com>
2019-02-28 06:42:44 +0100

    add layout

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,14 +1,22 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components";
+import Layout from "../components/layout";
 
 const Courses = ({ data }) => {
   if (!data && !data.markdownRemark) return <div>...loading</div>;
   const { markdownRemark: courses } = data;
 
   return (
-    <div>
+    <Layout>
+    <Container>
+      <h1> Předměty </h1>
+      <div>
       <div dangerouslySetInnerHTML={{ __html: courses.html }} />
     </div>
+    </Container>
+  </Layout>
+
   );
 };
 
@@ -23,4 +31,16 @@ export const coursesQuery = graphql`
       }
     }
   }
+`;
+
+const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  @media (max-width: 800px) {
+    width: 100%;
+  }
+`;
+
+const A = styled.a`
+  color: #333;
 `;
