b39894bc8dc5f7b5aa0461f9a4b093b3a8e7af95
Hubackova <lenka.h@atlas.cz>
2019-03-01 20:00:50 +0100

    images changes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import ecdyonurus from "../../pages/images/ecdyonurus_head.jpg";
+import ecdyonurus from "../../../static/images/ecdyonurus_head.jpg";
 import { Link } from "gatsby";
 const HeaderComponent = ({ className }) => {
   return (
