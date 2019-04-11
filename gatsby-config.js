e2e91f35e46f355dced408c0d9278c34cc44b1b3
Lenka <hubackova.lenka@gmail.com>
2019-04-11 23:06:54 +0200

    config fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,7 @@ module.exports = {
   },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content`,
-        name: "content"
-      }
-    },
+    
     {
       resolve: `gatsby-source-filesystem`,
       options: {
